84d32f84dd41ed46113f2c93f00012e1af804061
Paul Shannon <pshannon@sitepen.com>
2017-01-16 12:11:00 -0700

    Fixed cli support

--- src/cli.ts
+++ src/cli.ts
@@ -2,9 +2,11 @@ import WebServer from './WebServer';
 import ServeFile from './middleware/ServeFile';
 import ServeDirectory from './middleware/ServeDirectory';
 
-const server = new WebServer();
-server.app.middleware
-	.add(new ServeFile())
-	.add(new ServeDirectory());
-server.start();
-console.log(`started server on ${ server.config.port }`);
+export default function () {
+	const server = new WebServer();
+	server.app.middleware
+		.add(new ServeFile())
+		.add(new ServeDirectory());
+	server.start();
+	console.log(`started server on ${ server.config.port }`);
+}
