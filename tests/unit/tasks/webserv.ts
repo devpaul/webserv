e408b509acd23028c4d3fcf500f31e0ea6eeeaa9
Travis <support@sitepen.com>
2017-09-12 17:18:20 -0700

    test cleanup

--- tests/unit/tasks/webserv.ts
+++ tests/unit/tasks/webserv.ts
@@ -1,26 +1,42 @@
+import { assert } from 'chai';
 import registerSuite from 'intern/lib/interfaces/object';
 import * as webserv from 'src/tasks/webserv';
-import * as sinon from 'sinon';
+import { SinonStub, spy, stub } from 'sinon';
 
 let taskMock: any;
+let grunt: IGrunt;
 
 registerSuite('src/tasks/webserv', {
 	beforeEach() {
+		grunt = <any> {
+			event: {
+				emit: stub()
+			},
+			fail: {
+				warn: stub()
+			},
+			log: {
+				write: stub()
+			},
+			registerMultiTask: stub(),
+			registerTask: stub()
+		};
 		taskMock = {
 			data: { },
-			done: sinon.spy(function () {
-				return sinon.stub();
+			done: spy(function () {
+				return stub();
 			})
 		};
+		webserv.call(taskMock, grunt);
 	},
 
 	tests: {
 		construction: {
 			'default'() {
-				const grunt = {
-					registerMultiTask: sinon.stub()
-				};
-				webserv.call(taskMock, grunt);
+				assert.isTrue((<SinonStub> grunt.registerMultiTask).calledOnce);
+				assert.isTrue((<SinonStub> grunt.registerTask).calledOnce);
+				assert.strictEqual((<SinonStub> grunt.registerMultiTask).firstCall.args[0], 'webserv');
+				assert.strictEqual((<SinonStub> grunt.registerTask).firstCall.args[0], 'stopServers');
 			}
 		}
 	}
