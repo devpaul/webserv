a5b773295c42f32c5be111f66aa259e65ebd359f
Travis <support@sitepen.com>
2017-11-26 12:22:14 -0700

    example cleanup

--- examples/proxy.ts
+++ examples/proxy.ts
@@ -1,7 +1,6 @@
 import createServer from '../src/commands/createServer';
 import WebProxy from '../src/middleware/WebProxy';
 import { setLogLevel } from '../src/log';
-import { BasicServer } from '../src/servers/BasicServer';
 
 setLogLevel('debug');
 
@@ -10,22 +9,16 @@ const wsProxy = new WebProxy('http://echo.websocket.org', {
 	ws: true
 });
 
-function startServer(server: BasicServer) {
-	server.start()
-		.then(() => {
-			console.log('To use this example go to https://websocket.org/echo.html and');
-			console.log('connect to ws://localhost:7777');
-			console.log(`started server on ${ server.port }`);
-		});
-}
-
 // Create a http server at http://localhost:8888
 createServer({
 	middleware: [
 		wsProxy
 	],
 	port: 7777,
+	start: true,
 	upgrade: wsProxy
 }).then((server) => {
-	startServer(server);
+	console.log('To use this example go to https://websocket.org/echo.html and');
+	console.log('connect to ws://localhost:7777');
+	console.log(`started server on ${ server.port }`);
 });
