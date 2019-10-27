768c5e5a41cb9186d37612823a093a5339035836
Paul <paul@sitepen.com>
2019-10-26 17:38:48 -0700

    added basic log server integration test

---
+++ tests/integration/basic.test.ts
@@ -0,0 +1,26 @@
+/// <reference types="intern" />
+import { App } from '../../src/core/app';
+import { body } from '../../src/core/processors/body.processor';
+import { route } from '../../src/core/routes/route';
+import { response } from '../../src/core/middleware/response';
+import fetch from 'node-fetch';
+
+const { assert } = intern.getPlugin('chai');
+const { describe, it } = intern.getPlugin('interface.bdd');
+
+describe('basic server test', () => {
+	it('starts a log server', async () => {
+		const app = new App();
+		app.before.push(body({}));
+		app.routes.push(route({ middleware: response({ statusCode: 200 }) }));
+		const controls = await app.start('http', {
+			port: 3000
+		});
+		assert.isDefined(controls);
+
+		const result = await fetch('http://localhost:3000');
+		assert.strictEqual(result.status, 200);
+
+		await controls.stop();
+	});
+});
