bbb2f984857fd83ebd26fdcef29d0ec1c3dd6d39
Paul <paul@sitepen.com>
2019-12-20 12:55:00 -0700

    added tests

---
+++ src/config/services/crud.spec.ts
@@ -0,0 +1,61 @@
+/// <reference types="intern" />
+
+import { App } from '../../core/app';
+import { setupMocks, setupSinon } from '../../_support/mocks';
+
+const { assert } = intern.getPlugin('chai');
+const { describe, it, beforeEach, after } = intern.getPlugin('interface.bdd');
+
+describe('config/services/crud', () => {
+	describe('bootCrudService', () => {
+		const sinon = setupSinon();
+		const mockPathGuard = sinon.stub();
+		const mockCrudRoute = sinon.stub();
+		setupMocks({
+			'../../core/guards/path': { pathGuard: mockPathGuard },
+			'../../core/routes/crud.route': { crudRoute: mockCrudRoute }
+		});
+		let bootCrudService: typeof import('./crud').bootCrudService;
+
+		beforeEach(() => {
+			bootCrudService = require('./crud').bootCrudService;
+		});
+
+		after(() => {
+			sinon.restore();
+		});
+
+		function assertBoot(app: App, match: string, data?: any) {
+			assert.lengthOf(app.routes, 1);
+			assert.isTrue(mockPathGuard.calledOnce);
+			assert.isTrue(mockCrudRoute.calledOnce);
+			assert.deepEqual(mockPathGuard.firstCall.args[0], {
+				match
+			});
+			assert.deepEqual(mockCrudRoute.firstCall.args[0], { data });
+		}
+
+		it('creates a crud route', () => {
+			const app = new App();
+			const config = {
+				route: '*'
+			};
+
+			bootCrudService(app, config);
+
+			assertBoot(app, config.route);
+		});
+
+		it('creates a crud route with initial data', () => {
+			const app = new App();
+			const config = {
+				route: '*',
+				data: { 'id-1': 'one' }
+			};
+
+			bootCrudService(app, config);
+
+			assertBoot(app, config.route, config.data);
+		});
+	});
+});
---
+++ src/config/services/proxy.spec.ts
@@ -0,0 +1,21 @@
+/// <reference types="intern" />
+
+import { bootProxyService } from './proxy';
+import { App } from '../../core/app';
+
+const { assert } = intern.getPlugin('chai');
+const { describe, it } = intern.getPlugin('interface.bdd');
+
+describe('config/services/proxy', () => {
+	describe('bootProxyService', () => {
+		it('Adds a route and an upgrader', () => {
+			const app = new App();
+			const config = {
+				target: 'https://example.org'
+			};
+			bootProxyService(app, config);
+			assert.lengthOf(app.routes, 1);
+			assert.isDefined(app.upgrader);
+		});
+	});
+});
