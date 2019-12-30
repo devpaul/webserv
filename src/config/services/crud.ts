0b5d3c5962d470ae48879df6a6d28dcfba89cefb
Paul <paul@sitepen.com>
2019-12-30 00:38:57 -0700

    remove crud route for crud service

--- src/config/services/crud.ts
+++ src/config/services/crud.ts
@@ -1,18 +1,11 @@
-import { App } from '../../core/app';
-import { pathGuard } from '../../core/guards/path';
-import { crudRoute, CrudProperties } from '../../core/routes/crud.route';
-import { route } from '../../core/routes/route';
+import { crudService, CrudServiceProperties } from '../../core/services/crud.service';
 
-export interface CrudConfig extends CrudProperties {
+import { App } from '../../core/app';
+
+export interface CrudConfig extends CrudServiceProperties {
 	path: string;
 }
 
 export function bootCrudService(app: App, config: CrudConfig) {
-	const { path: match, data } = config;
-	app.routes.push(
-		route({
-			guards: [pathGuard({ match })],
-			middleware: [crudRoute({ data })]
-		})
-	);
+	app.addService(crudService(config));
 }
