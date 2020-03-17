8f34f46653f9c4c9d3fbdfc99a1a795835ce55f2
Paul <paul@sitepen.com>
2020-03-16 21:36:09 -0700

    Make CRUD operations optional

--- src/core/services/crud.service.ts
+++ src/core/services/crud.service.ts
@@ -5,11 +5,15 @@ import { body } from '../processors/body.processor';
 import { jsonTransform } from '../transforms/json.transform';
 import { getParams } from '../util/request';
 import { pathGuard } from '../guards/path';
+import { RouteDescriptor } from '../interface';
 
 export interface CrudServiceProperties {
 	path?: string;
 	data?: { [key: string]: any };
+	operations?: Operation[];
 }
+
+export type Operation = 'list' | 'create' | 'get' | 'delete' | 'put';
 
 interface Record {
 	id: string;
@@ -21,69 +25,72 @@ function isRecord(value: any): value is Record {
 }
 
 export function crudService(props: CrudServiceProperties): Service {
-	const { path = '*', data = {} } = props;
+	const { path = '*', data = {}, operations = ['list', 'create', 'get', 'delete', 'put'] } = props;
 	const store: { [key: string]: any } = data;
+	const middlewares: { [P in Operation]: RouteDescriptor } = {
+		list: {
+			guards: [method.get()],
+			middleware: () => {
+				return store;
+			}
+		},
+		create: {
+			guards: [method.post()],
+			middleware: (request) => {
+				const { body } = getParams(request, 'body');
+				if (isRecord(body)) {
+					store[body.id] = body;
+				} else {
+					throw new HttpError(HttpStatus.BadRequest);
+				}
+			}
+		},
+		get: {
+			guards: [method.get('/id/:id')],
+			middleware: (request) => {
+				const { params } = getParams(request, 'params');
+				if (params.id && store[params.id]) {
+					return store[params.id];
+				}
+				throw new HttpError(HttpStatus.NotFound);
+			}
+		},
+		delete: {
+			guards: [method.delete('/id/:id')],
+			middleware: (request) => {
+				const { params } = getParams(request, 'params');
+				if (params.id && store[params.id]) {
+					const record = store[params.id];
+					delete store[params.id];
+					return record;
+				}
+				throw new HttpError(HttpStatus.NotFound);
+			}
+		},
+		put: {
+			guards: [method.put()],
+			middleware: (request) => {
+				const { body } = getParams(request, 'body');
+				if (isRecord(body)) {
+					store[body.id] = {
+						...store[body.id],
+						...body
+					};
+					return store[body.id];
+				}
+				throw new HttpError(HttpStatus.NotFound);
+			}
+		}
+	};
 
 	return {
 		route: {
 			guards: [pathGuard({ match: path })],
 			before: [body({})],
 			transforms: [jsonTransform],
-			middleware: [
-				{
-					guards: [method.get()],
-					middleware: () => {
-						return store;
-					}
-				},
-				{
-					guards: [method.post()],
-					middleware: (request) => {
-						const { body } = getParams(request, 'body');
-						if (isRecord(body)) {
-							store[body.id] = body;
-						} else {
-							throw new HttpError(HttpStatus.BadRequest);
-						}
-					}
-				},
-				{
-					guards: [method.get('/id/:id')],
-					middleware: (request) => {
-						const { params } = getParams(request, 'params');
-						if (params.id && store[params.id]) {
-							return store[params.id];
-						}
-						throw new HttpError(HttpStatus.NotFound);
-					}
-				},
-				{
-					guards: [method.delete('/id/:id')],
-					middleware: (request) => {
-						const { params } = getParams(request, 'params');
-						if (params.id && store[params.id]) {
-							const record = store[params.id];
-							delete store[params.id];
-							return record;
-						}
-						throw new HttpError(HttpStatus.NotFound);
-					}
-				},
-				{
-					guards: [method.put()],
-					middleware: (request) => {
-						const { body } = getParams(request, 'body');
-						if (isRecord(body)) {
-							store[body.id] = {
-								...store[body.id],
-								...body
-							};
-							return store[body.id];
-						}
-						throw new HttpError(HttpStatus.NotFound);
-					}
-				}
-			]
+			middleware: operations.map((op) => {
+				return middlewares[op as Operation];
+			})
 		}
 	};
 }
