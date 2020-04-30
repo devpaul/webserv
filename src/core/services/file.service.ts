0ae6e3c78438cda44b32a1dfae728947c0f214d5
Paul Shannon <paul@devpaul.com>
2020-04-29 21:47:04 -0700

    file - routes

--- src/core/services/file.service.ts
+++ src/core/services/file.service.ts
@@ -1,21 +1,20 @@
 import { Service } from '../app';
 import { method } from '../guards/method';
+import { RouteProperties } from '../interface';
 import { serve, ServeProperties } from '../middleware/serve';
 import { noCache } from '../processors/cache.processor';
 import { directoryTransform } from '../transforms/directory.transform';
 import { jsonTransform } from '../transforms/json.transform';
 
-export interface FileServiceProperties extends ServeProperties {
-	path?: string;
-}
+export interface FileServiceProperties extends ServeProperties, RouteProperties {}
 
 export function fileService(props: FileServiceProperties): Service {
-	const { path = '*' } = props;
+	const { route = '*' } = props;
 
 	return {
 		route: {
 			before: [noCache],
-			guards: [method.get(path)],
+			guards: [method.get(route)],
 			middleware: serve(props),
 			transforms: [directoryTransform, jsonTransform]
 		}
