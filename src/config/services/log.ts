5ce026659ff3507071f6bd80ee426c23c4eb47ee
Paul Shannon <paul@devpaul.com>
2020-04-29 21:52:25 -0700

    updated log

--- src/config/services/log.ts
+++ src/config/services/log.ts
@@ -1,18 +1,15 @@
-import { setLogLevel } from '../../core/log';
 import { response } from '../../core/middleware/response';
 import { body } from '../../core/processors/before/body.processor';
 import { logRequest } from '../../core/processors/log.processor';
 import { SimpleServiceLoader } from '../loader';
 
 export interface LogConfig {
-	level?: string;
 	respondOk?: boolean;
 }
 
 export const bootLogService: SimpleServiceLoader<LogConfig> = (config) => {
-	const { level, respondOk } = config;
+	const { respondOk } = config;
 
-	level && setLogLevel(level);
 	return {
 		global: {
 			before: [body({}), logRequest({ logBody: true })]
